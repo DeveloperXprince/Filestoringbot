60badbf668430070c4195f357dae600c6f28c552
Prince <109408511+DeveloperXprince@users.noreply.github.com>
2023-08-23 21:12:09 +0530

    Create configs.py

---
+++ configs.py
@@ -0,0 +1,58 @@
+# (c) @Altronchats || @Thealtron
+
+import os
+
+
+class Config(object):
+	API_ID = int(os.environ.get("API_ID", "0"))
+	API_HASH = os.environ.get("API_HASH")
+	BOT_TOKEN = os.environ.get("BOT_TOKEN")
+	BOT_USERNAME = os.environ.get("BOT_USERNAME")
+	DB_CHANNEL = int(os.environ.get("DB_CHANNEL", "-100"))
+	BOT_OWNER = int(os.environ.get("BOT_OWNER", "1445283714"))
+	DATABASE_URL = os.environ.get("DATABASE_URL")
+	UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "")
+	LOG_CHANNEL = os.environ.get("LOG_CHANNEL", None)
+	BANNED_USERS = set(int(x) for x in os.environ.get("BANNED_USERS", "1234567890").split())
+	FORWARD_AS_COPY = bool(os.environ.get("FORWARD_AS_COPY", True))
+	BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST_AS_COPY", False))
+	BANNED_CHAT_IDS = list(set(int(x) for x in os.environ.get("BANNED_CHAT_IDS", "-1001362659779 -1001255795497").split()))
+	OTHER_USERS_CAN_SAVE_FILE = bool(os.environ.get("OTHER_USERS_CAN_SAVE_FILE", True))
+	ABOUT_BOT_TEXT = f"""
+This is  Prince and i am Permanent FileStoring Bot. 
+Send Me any Media or File.I can Work In Channel too Add Me to Channel with Edit Permission, I will add save Uploaded File in Channel and Share a Shareable Link. 
+
+╭────[ **🔅FɪʟᴇSᴛᴏʀᴇBᴏᴛ🔅**]────⍟
+│
+├🔸🤖 **My Name:** [Prince ✨i](https://t.me/{BOT_USERNAME})
+│
+├🔸📝 **Language:** [𝐏𝐲𝐭𝐡𝐨𝐧𝟑](https://www.python.org)
+│
+├🔹📚 **Library:** [𝐏𝐲𝐫𝐨𝐠𝐫𝐚𝐦](https://docs.pyrogram.org)
+│
+├🔹📡 **Hosted On:** [𝐇𝐞𝐫𝐨𝐤𝐮](https://heroku.com)
+│
+├🔸👨‍💻 **Developer:** [@detective_prince](https://t.me/detective_prince) 
+│
+├🔹👥 **Bot Support:** [𝐒𝐮𝐩𝐩𝐨𝐫𝐭](https://t.me/Altronchats)
+│
+├🔸🔔 **Bot Updates:** [𝐔𝐩𝐝𝐚𝐭𝐞𝐬](AltronChats)
+│
+╰──────[ 😎 ]───────────⍟
+"""
+	ABOUT_DEV_TEXT = f"""
+🧑🏻‍💻 **𝗗𝗲𝘃𝗲𝗹𝗼𝗽𝗲𝗿:** [@detective_prince](https://t.me/detective_prince) 
+
+𝐃𝐞𝐯𝐞𝐥𝐨𝐩𝐞𝐫 𝐢𝐬 𝐒𝐮𝐩𝐞𝐫 𝐍𝐨𝐨𝐛. 𝐉𝐮𝐬𝐭 𝐋𝐞𝐚𝐫𝐧𝐢𝐧𝐠 𝐟𝐫𝐨𝐦 𝐎𝐟𝐟𝐢𝐜𝐢𝐚𝐥 𝐃𝐨𝐜𝐬. 𝐀𝐧𝐝 𝐒𝐞𝐞𝐤𝐢𝐧𝐠 𝐇𝐞𝐥𝐩 𝐅𝐫𝐨𝐦 𝐏𝐫𝐨 𝐂𝐨𝐝𝐞𝐫𝐬\n**@Altronchats**
+
+𝐈𝐟 𝐘𝐨𝐮 𝐰𝐚𝐧𝐭 𝐭𝐨 𝐃𝐨𝐧𝐚𝐭𝐞 𝐎𝐮𝐫 𝐇𝐚𝐫𝐝 𝐖𝐨𝐫𝐤. 𝐘𝐨𝐮 𝐂𝐚𝐧 𝐂𝐨𝐧𝐭𝐚𝐜𝐭 𝐓𝐡𝐞 𝐃𝐞𝐯𝐞𝐥𝐨𝐩𝐞𝐫. 
+
+𝐀𝐥𝐬𝐨 𝐫𝐞𝐦𝐞𝐦𝐛𝐞𝐫 𝐭𝐡𝐚𝐭 𝐝𝐞𝐯𝐞𝐥𝐨𝐩𝐞𝐫 𝐰𝐢𝐥𝐥 𝐃𝐞𝐥𝐞𝐭𝐞 𝐀𝐝𝐮𝐥𝐭 𝐂𝐨𝐧𝐭𝐞𝐧𝐭𝐬 𝐟𝐫𝐨𝐦 𝐃𝐚𝐭𝐚𝐛𝐚𝐬𝐞. 𝐒𝐨 𝐛𝐞𝐭𝐭𝐞𝐫 𝐝𝐨𝐧'𝐭 𝐒𝐭𝐨𝐫𝐞 𝐓𝐡𝐨𝐬𝐞 𝐊𝐢𝐧𝐝 𝐨𝐟 𝐓𝐡𝐢𝐧𝐠𝐬.
+
+[**Donate Me**](t.me/detective_prince) (contact him ✨)
+"""
+	HOME_TEXT = """
+Hello [{}](tg://user?id={})\n\n 
+
+I can store private files in Specified Channel and other users can access it from special link. ✨🔥
+"""
